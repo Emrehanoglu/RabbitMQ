bc325fbf528be0197d01651c65dfbec86b8f2d00
Emrehanoglu <72378457+Emrehanoglu@users.noreply.github.com>
2024-06-29 16:47:01 +0300

    Add project files.

---
+++ RabbitMQ.publisher/Program.cs
@@ -0,0 +1,45 @@
+﻿using RabbitMQ.Client;
+using System.Text;
+
+//baglantıyı gercekleştirildi
+var factory = new ConnectionFactory();
+factory.Uri = new Uri("amqps://teslduyy:ESr7N7TN_h5rGVk75-WpqGkmzTrceyBp@moose.rmq.cloudamqp.com/teslduyy");
+
+//baglantı acıldı
+using var connection = factory.CreateConnection();
+
+//kanal oluşturuldu
+//bu kanal üzerinden artık rabbitMq ile haberlesme saglanacak
+var channel = connection.CreateModel();
+
+//haberlesme saglanması icin mevcutta bir kuyrugun olması gerekiyor
+//kuyruk olusturulurken girilen parametreler sırasıyla,
+
+//1. parametre queue --> kuyruk adı
+
+//2. parametre durable --> rabbitMq 'ya restart atıldıgında içerisindeki datalar hafızaya kaydedilir ve kaybolmamış olur.
+//true : hafızaya kaydetme, datalar silinsin, false : hafızaya kaydet, datalar silinmesin
+
+//3. parametre exclusive --> olusturulan bu kuyruga farklı kanallar üzerinden de baglanılabilsin mi?
+//true : sadece bu kanal baglanalabilsin, false : farklı kanallarda baglanalabilsin
+
+//4. parametre autodelete --> kuyrugu dinleyen subscriber 'ın iletişimi koparsa ve kuyrugu
+//dinleyen bir subscribe kalmaz ise kuyrukta silinsin mi?
+//true : evet silinsin, false : silinmesin, kuyruk her zaman ayakta kalsın
+
+channel.QueueDeclare("hello-queue", true, false, false);
+
+//kuyruga bırakılacak mesaj oluşturuldu.
+string message = "hello world";
+
+//rabbitMq 'ya veriler byte olarak gönderilir
+var messageBody = Encoding.UTF8.GetBytes(message);
+
+//mesajı kuyruga bırabilirim artık.
+
+//exchange kullanmadıgım için 1. parametrem string.Empty ve 2. parametrem de direkt olarak
+//kuyrugun adı.
+channel.BasicPublish(string.Empty, "hello-queue", null, messageBody);
+
+Console.WriteLine("Mesaj gönderilmiştir.");
+Console.ReadLine();
---
+++ RabbitMQ.subscriber/Program.cs
@@ -0,0 +1,57 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+//baglantıyı gercekleştirildi
+var factory = new ConnectionFactory();
+factory.Uri = new Uri("amqps://teslduyy:ESr7N7TN_h5rGVk75-WpqGkmzTrceyBp@moose.rmq.cloudamqp.com/teslduyy");
+
+//baglantı acıldı
+using var connection = factory.CreateConnection();
+
+//kanal oluşturuldu
+//bu kanal üzerinden artık rabbitMq ile haberlesme saglanacak
+var channel = connection.CreateModel();
+
+//haberlesme saglanması icin mevcutta bir kuyrugun olması gerekiyor
+//kuyruk olusturulurken girilen parametreler sırasıyla,
+
+//1. parametre queue --> kuyruk adı
+
+//2. parametre durable --> rabbitMq 'ya restart atıldıgında içerisindeki datalar hafızaya kaydedilir ve kaybolmamış olur.
+//true : hafızaya kaydetme, datalar silinsin, false : hafızaya kaydet, datalar silinmesin
+
+//3. parametre exclusive --> olusturulan bu kuyruga farklı kanallar üzerinden de baglanılabilsin mi?
+//true : sadece bu kanal baglanalabilsin, false : farklı kanallarda baglanalabilsin
+
+//4. parametre autodelete --> kuyrugu dinleyen subscriber 'ın iletişimi koparsa ve kuyrugu
+//dinleyen bir subscribe kalmaz ise kuyrukta silinsin mi?
+//true : evet silinsin, false : silinmesin, kuyruk her zaman ayakta kalsın
+
+//publisher tarafında zaten bu kuyruk oldugu için subscriber tarafında
+//kuyrugun tekrar olusturulmasına gerek yok.
+//channel.QueueDeclare("hello-queue", true, false, false);
+
+//kuyruktaki mesajı okuyacak olan yapı
+var consumer = new EventingBasicConsumer(channel);
+
+//consume hangi kuyrugu okuyacak
+
+//1. parametre kuyrugun adı,
+
+//2. parametre ile subscriber tarafından alınan data doğruda işlense yanlış da işlense
+//kuyruk üzerinde kalmaya devam etsin mi ?
+//true : mesajı okudugun an sil, false : mesajı silme, dogru bir sekilde islenirse silmen
+//için haber vereceğim
+
+//3. parametre ile kuyruktaki mesajı okuyacak olan yapıyı veriyorum
+channel.BasicConsume("hello-queue", true, consumer);
+
+//mesajı artık okuyabilirim
+consumer.Received += (sender, args) =>
+{
+    var message = Encoding.UTF8.GetString(args.Body.ToArray());
+    Console.WriteLine("Gelen Mesaj: " + message);
+};
+
+Console.ReadLine();
