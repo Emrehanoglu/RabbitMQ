d11c00cead03da0fee277cca88f64e77fd1aa00f
Emrehanoglu <72378457+Emrehanoglu@users.noreply.github.com>
2024-07-09 22:19:55 +0300

    RabbitMQ #18

--- RabbitMQWeb.ExcelCreate/Controllers/ProductController.cs
+++ RabbitMQWeb.ExcelCreate/Controllers/ProductController.cs
@@ -1,13 +1,59 @@
 ﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using RabbitMQWeb.ExcelCreate.Models;
 
 namespace RabbitMQWeb.ExcelCreate.Controllers;
 
 [Authorize]
 public class ProductController : Controller
 {
+    private readonly UserManager<IdentityUser> _userManager;
+    private readonly AppDbContext _appDbContext;
+
+    public ProductController(UserManager<IdentityUser> userManager, AppDbContext appDbContext)
+    {
+        _userManager = userManager;
+        _appDbContext = appDbContext;
+    }
+
     public IActionResult Index()
     {
         return View();
+    }
+    public async Task<IActionResult> CreateProductExcel()
+    {
+        //önce kullanıcıyı bulalım
+        var user = await _userManager.FindByNameAsync(User.Identity.Name);
+
+        //kullanıcı bulundu, dosya oluşturalım
+        var fileName = $"product-excel-{Guid.NewGuid().ToString().Substring(1,10)}";
+
+        UserFile userFile = new()
+        {
+            UserId = user.Id,
+            FileName = fileName,
+            FileStatus = FileStatus.Creating
+        };
+
+        //veritabanında bu dosyayı oluşturalım
+        await _appDbContext.UserFiles.AddAsync(userFile);
+
+        await _appDbContext.SaveChangesAsync();
+
+        //RabbitMq'ya mesaj gönderildi.
+        TempData["StartCreatingExcel"] = true;
+
+        return RedirectToAction("Files","Product");
+    }
+
+    public async Task<IActionResult> Files()
+    {
+        //önce kullanıcıyı bulalım
+        var user = await _userManager.FindByNameAsync(User.Identity.Name);
+
+        //kullanıcıya ait dosyaları bulup return edelim
+        return View(await _appDbContext.UserFiles.Where(x => x.UserId == user.Id).ToListAsync());
     }
 }
--- RabbitMQWeb.ExcelCreate/Migrations/20240709191413_mig2.Designer.cs
+++ RabbitMQWeb.ExcelCreate/Migrations/20240709191413_mig2.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using RabbitMQWeb.ExcelCreate.Models;
 
@@ -11,9 +12,10 @@ using RabbitMQWeb.ExcelCreate.Models;
 namespace RabbitMQWeb.ExcelCreate.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240709191413_mig2")]
+    partial class mig2
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
