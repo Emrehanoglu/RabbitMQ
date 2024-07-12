8720ff0ee8a826484049d2b9cd8264864103f6c9
Emrehanoglu <72378457+Emrehanoglu@users.noreply.github.com>
2024-07-13 00:19:33 +0300

    RabbitMQ #26

---
+++ RabbitMQ.Tekrar/FanoutExchange/RabbitMQ.publisher/Program.cs
@@ -0,0 +1,22 @@
+﻿using RabbitMQ.Client;
+using System.Text;
+
+var factory = new ConnectionFactory();
+factory.Uri = new Uri("amqps://teslduyy:ESr7N7TN_h5rGVk75-WpqGkmzTrceyBp@moose.rmq.cloudamqp.com/teslduyy");
+
+var connection = factory.CreateConnection();
+
+var channel = connection.CreateModel();
+
+channel.ExchangeDeclare("tekrar-fanout", ExchangeType.Fanout, true, false);
+
+Enumerable.Range(1, 25).ToList().ForEach(x =>
+{
+    var message = $"mesaj - {x}";
+    var messageBody = Encoding.UTF8.GetBytes(message);
+
+    channel.BasicPublish("tekrar-fanout","",null,messageBody);
+
+    Console.WriteLine($"Mesaj gönderilmiştir - {message}");
+});
+Console.ReadLine();
---
+++ RabbitMQ.Tekrar/FanoutExchange/RabbitMQ.subscriber/Program.cs
@@ -0,0 +1,37 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+var factory = new ConnectionFactory();
+factory.Uri = new Uri("amqps://teslduyy:ESr7N7TN_h5rGVk75-WpqGkmzTrceyBp@moose.rmq.cloudamqp.com/teslduyy");
+
+var connection = factory.CreateConnection();
+
+var channel = connection.CreateModel();
+
+channel.BasicQos(0,1,false);
+
+var queueName = channel.QueueDeclare().QueueName;
+
+channel.QueueBind(queueName,"tekrar-fanout","",null);
+
+var consumer = new EventingBasicConsumer(channel);
+
+channel.BasicConsume(queueName,false,consumer);
+
+Console.WriteLine("mesajlar okunuyor...");
+
+consumer.Received += Consumer_Received;
+
+void Consumer_Received(object? sender, BasicDeliverEventArgs e)
+{
+    var message = Encoding.UTF8.GetString(e.Body.ToArray());
+
+    Thread.Sleep(1000);
+
+    Console.WriteLine($"Okunan mesaj - {message}");
+
+    channel.BasicAck(e.DeliveryTag, false);
+}
+
+Console.ReadLine();
