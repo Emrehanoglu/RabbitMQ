e002be575bdaa8f4f11e43955b77d96e7a2b4a78
Emrehanoglu <72378457+Emrehanoglu@users.noreply.github.com>
2024-07-12 23:01:51 +0300

    RabbitMQ #25

---
+++ RabbitMQ.Tekrar/NoExchange/RabbitMQ.subscriber/Program.cs
@@ -0,0 +1,25 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+var factory = new ConnectionFactory();
+factory.Uri = new Uri("amqps://teslduyy:ESr7N7TN_h5rGVk75-WpqGkmzTrceyBp@moose.rmq.cloudamqp.com/teslduyy");
+
+var connection = factory.CreateConnection();
+
+var channel = connection.CreateModel();
+
+channel.BasicQos(0,1,false);
+
+var consumer = new EventingBasicConsumer(channel);
+
+channel.BasicConsume("merhaba-queue", false, consumer);
+
+consumer.Received += Consumer_Received;
+
+void Consumer_Received(object? sender, BasicDeliverEventArgs e)
+{
+    var message = Encoding.UTF8.GetString(e.Body.ToArray());
+    Console.WriteLine("Okunan Mesaj : " + message);
+}
+Console.ReadLine();
---
+++ RabbitMQ.Tekrar/RabbitMQ.publisher/Program.cs
@@ -0,0 +1,18 @@
+﻿
+using RabbitMQ.Client;
+using System.Text;
+
+var factory = new ConnectionFactory();
+factory.Uri = new Uri("amqps://teslduyy:ESr7N7TN_h5rGVk75-WpqGkmzTrceyBp@moose.rmq.cloudamqp.com/teslduyy");
+
+var connection = factory.CreateConnection();
+
+var channel = connection.CreateModel();
+
+channel.QueueDeclare("merhaba-queue",durable:true,false,false);
+
+var message = "merhaba dünya";
+
+var messageBody = Encoding.UTF8.GetBytes(message);
+
+channel.BasicPublish(string.Empty, "merhaba-queue", null, messageBody);
