ccc5867f1ae088213ba0335c00491c3defe9f60f
Emrehanoglu <72378457+Emrehanoglu@users.noreply.github.com>
2024-07-13 03:59:00 +0300

    RabbitMQ #27

---
+++ RabbitMQ.Tekrar/DirectExchange/RabbitMQ.publisher/Program.cs
@@ -0,0 +1,43 @@
+﻿using RabbitMQ.Client;
+using System.Text;
+
+var factory = new ConnectionFactory();
+factory.Uri = new Uri("amqps://teslduyy:ESr7N7TN_h5rGVk75-WpqGkmzTrceyBp@moose.rmq.cloudamqp.com/teslduyy");
+
+var connection = factory.CreateConnection();
+
+var channel = connection.CreateModel();
+
+channel.ExchangeDeclare("tekrar-direct-exchange",ExchangeType.Direct,true,false);
+
+Enum.GetNames(typeof(MessageNames)).ToList().ForEach(x =>
+{
+    var queueName = $"tekrar-direct-queue-{x}";
+    channel.QueueDeclare(queueName,true,false,false);
+    
+    var routeKey = $"route-{x}";
+
+    channel.QueueBind(queueName, "tekrar-direct-exchange",routeKey);
+});
+
+Enumerable.Range(1, 50).ToList().ForEach(x =>
+{
+    MessageNames messageType = (MessageNames)new Random().Next(1,5);
+
+    var message = $"{x}. message-{messageType}";
+    var messageBody = Encoding.UTF8.GetBytes(message);
+
+    var route = $"route-{messageType}";
+
+    channel.BasicPublish("tekrar-direct-exchange",route,null, messageBody);
+    Console.WriteLine($"Message sended : {message}");
+});
+Console.ReadLine();
+
+public enum MessageNames
+{
+    Critical = 1,
+    Error = 2,
+    Warning = 3,
+    Info = 4
+}
---
+++ RabbitMQ.Tekrar/DirectExchange/RabbitMQ.subscriber/Program.cs
@@ -0,0 +1,33 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+var factory = new ConnectionFactory();
+factory.Uri = new Uri("amqps://teslduyy:ESr7N7TN_h5rGVk75-WpqGkmzTrceyBp@moose.rmq.cloudamqp.com/teslduyy");
+
+var connection = factory.CreateConnection();
+
+var channel = connection.CreateModel();
+
+channel.BasicQos(0,1,false);
+
+var consumer = new EventingBasicConsumer(channel);
+
+var queuename = "tekrar-direct-queue-Info";
+
+channel.BasicConsume(queuename,false,consumer);
+
+consumer.Received += Consumer_Received;
+
+void Consumer_Received(object? sender, BasicDeliverEventArgs e)
+{
+    var message = Encoding.UTF8.GetString(e.Body.ToArray());
+
+    Thread.Sleep(500);
+
+    Console.WriteLine(message);
+
+    channel.BasicAck(e.DeliveryTag,false);
+}
+
+Console.ReadLine();
